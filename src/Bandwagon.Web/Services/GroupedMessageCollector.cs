11d322a2005af620221f8e1331a7e9872eee8303
Zach Starkebaum <zach@relevantwalk.com>
2023-04-05 01:46:18 -0700

    Grouping

--- src/Bandwagon.Web/Services/GroupedMessageCollector.cs
+++ src/Bandwagon.Web/Services/GroupedMessageCollector.cs
@@ -4,24 +4,48 @@ namespace Bandwagon.Web.Services;
 
 public class GroupedMessageCollector
 {
+    private readonly object _messageLocker = new();
+    private readonly object _groupLocker = new();
+
     private readonly MessageCorrelator _messageCorrelator;
 
-    List<IChatMessage> _recentMessages = new();
-    public List<IChatMessage> RecentMessages => _recentMessages;
+    List<CollectedMessage> _recentMessages = new(5000);
+    public List<CollectedMessage> RecentMessages
+    {
+        get
+        {
+            lock(_messageLocker)
+            {
+                return _recentMessages.TakeLast(100).ToList();
+            }
+        }
+    }
 
-    List<MessageGroup> _groups = new();
-    public List<MessageGroup> Groups => _groups;
-
-    List<MessageGroup> _oldGroups = new();
-    public List<MessageGroup> OldGroups => _oldGroups;
+    List<MessageGroup> _groups = new(500);
+    public List<MessageGroup> Groups
+    {
+        get
+        {
+            lock(_groupLocker)
+            {
+                var groupCompareStart = _lastRecievedTs - GroupCooldownSeconds * 1000;
+                return _groups
+                    .Where(g => g.LastMessageAddedAt > groupCompareStart)
+                    .OrderByDescending(g => g.Messages.Count)
+                    .Take(LimitGroupCount)
+                    .OrderBy(g => g.FirstMessageAt)
+                    .ToList();
+            }
+        }
+    }
 
     long? _lastRecievedTs;
 
-    public List<IChatMessage> ChatLogWithoutGroups { get; private set; } = new();
     public double ConsiderMessageForGroupingWindowSeconds { get; set; } = 10.0;
     public double GroupCooldownSeconds { get; set; } = 10.0;
     public int MinimumRelatedMessagesToCreateNewGroup { get; set; } = 3;
     public double MessageDistanceThreshold { get; set; } = 0.5;
+    public int LimitGroupCount { get; set; } = 3;
 
     public GroupedMessageCollector(MessageCorrelator messageCorrelator)
     {
@@ -30,76 +54,96 @@ public class GroupedMessageCollector
 
     public void Reset()
     {
-        _recentMessages.Clear();
-        ReplaceGroups(new());
-        _oldGroups.Clear();
+        lock(_messageLocker)
+        {
+            _recentMessages.Clear();
+        }
+        lock(_groupLocker)
+        {
+            _groups.Clear();
+        }
     }
 
     public void ReceiveMessage(IChatMessage newMessage)
     {
         _lastRecievedTs = newMessage.TmiSentTs;
-        var windowStart = _lastRecievedTs - ConsiderMessageForGroupingWindowSeconds * 1000;
 
-        _recentMessages = _recentMessages.Where(m => m.TmiSentTs > windowStart).ToList();
+        var message = new CollectedMessage(newMessage);
 
-        var cooldownThresholdTime = newMessage.TmiSentTs - GroupCooldownSeconds * 1000;
-
-        var newGroups = _groups.ToList();
-
-        foreach (var group in newGroups.ToList())
+        if (newMessage.Username == "Nightbot" || newMessage.Message.Trim().StartsWith("!"))
         {
-            if (group.LastMessageAddedAt < cooldownThresholdTime)
-            {
-                _oldGroups.Add(group);
-                newGroups.Remove(group);
-                continue;
-            }
+            _recentMessages.Add(message);
+            return;
         }
 
         // Check to see if the message fits into any of the current groups.
-        foreach (var group in newGroups.ToList())
+        var groupCompareStart = newMessage.TmiSentTs - GroupCooldownSeconds * 1000;
+        lock (_groupLocker)
         {
-            var updatedGroup = _messageCorrelator.TryCorrelateWithGroup(
-                newMessage,
-                group,
-                MessageDistanceThreshold);
-
-            if (updatedGroup is not null)
+            var groupsForComparison = _groups.Where(g => g.LastMessageAddedAt > groupCompareStart).ToList();
+            foreach (var group in groupsForComparison)
             {
-                newGroups[newGroups.IndexOf(group)] = updatedGroup;
-                ReplaceGroups(newGroups);
-                return;
+                var didCorrelate = _messageCorrelator.TryCorrelateWithGroup(
+                    message,
+                    group,
+                    MessageDistanceThreshold);
+
+                if (didCorrelate)
+                {
+                    _recentMessages.Add(message);
+                    return;
+                }
             }
+        }
+
+        var compareWindowStart = _lastRecievedTs - ConsiderMessageForGroupingWindowSeconds * 1000;
+        List<CollectedMessage> msgsForComparison;
+        lock (_messageLocker)
+        {
+            msgsForComparison = _recentMessages
+                .Where(m => m.Group is null)
+                .Where(m => m.Message.TmiSentTs > compareWindowStart)
+                .ToList();
         }
 
         // Otherwise check to see if this message might start a new group.
         var newGroup = _messageCorrelator.CorrelateNewMessage(
-            newMessage,
-            _recentMessages,
+            message,
+            msgsForComparison,
             MessageDistanceThreshold,
             MinimumRelatedMessagesToCreateNewGroup);
         if (newGroup is not null)
         {
-            foreach (var m in newGroup.Messages.Select(m => m.Message))
-                _recentMessages.Remove(m);
-
-            newGroups.Add(newGroup);
-            ReplaceGroups(newGroups);
-            return;
+            lock(_groupLocker)
+                _groups.Add(newGroup);
         }
 
-        _recentMessages.Add(newMessage);
-    }
-
-    private void ReplaceGroups(List<MessageGroup> groups)
-    {
-        Interlocked.Exchange(ref _groups, groups);
+        _recentMessages.Add(message);
     }
 }
 
-public record MessageGroup(IChatMessage RepresentativeMessage, List<ScoredMessage> Messages, long FirstMessageAt, long LastMessageAddedAt)
+public class MessageGroup
 {
-    public Guid Id { get; } = Guid.NewGuid();
+    public List<CollectedMessage> Messages { get; set; }
+    public long FirstMessageAt { get; set; }
+    public long LastMessageAddedAt { get; set; }
+
+    public MessageGroup(List<CollectedMessage> messages, long firstMessageAt, long lastMessageAddedAt)
+    {
+        Messages = messages;
+        FirstMessageAt = firstMessageAt;
+        LastMessageAddedAt = lastMessageAddedAt;
+    }
 }
 
-public record ScoredMessage(IChatMessage Message, double? Score);
+public class CollectedMessage
+{
+    public IChatMessage Message { get; set; }
+    public MessageGroup? Group { get; set; }
+
+    public CollectedMessage(IChatMessage message)
+    {
+        Message = message;
+    }
+}
+
--- src/Bandwagon.Web/Services/MessageCorrelator.cs
+++ src/Bandwagon.Web/Services/MessageCorrelator.cs
@@ -1,58 +1,69 @@
-﻿using Bandwagon.Web.Models;
-using F23.StringSimilarity;
+﻿using F23.StringSimilarity;
 using F23.StringSimilarity.Interfaces;
 
 namespace Bandwagon.Web.Services;
 
 public class MessageCorrelator
 {
-    // Most relevant: has the most occurrences of the lowest distance. (can probably just add the distances?)
-    // Group, all the messages relate to all the other messages
-
     //var algorithm = new NGram(4);
-    private INormalizedStringDistance _algorithm = new MetricLCS();
     //var algorithm = new LongestCommonSubsequence();
     //var algorithm = new QGram(4);
+    private readonly INormalizedStringDistance _algorithm = new MetricLCS();
 
     public MessageGroup? CorrelateNewMessage(
-        IChatMessage testMessage, List<IChatMessage> messages, double distanceThreshold, int countThreshold)
+        CollectedMessage testMessage, List<CollectedMessage> toCompare, double distanceThreshold, int countThreshold)
     {
-        var matches = messages
-            .Select(m => new ScoredMessage(m, _algorithm.Distance(testMessage.Message.ToLowerInvariant(), m.Message.ToLowerInvariant())))
-            .Where(ms => ms.Score <= distanceThreshold)
+        var matches = toCompare
+            .Where(m => Distance(testMessage, m) <= distanceThreshold)
             .ToList();
+
+        // Counts must be distinct by username before grouping
+        if (matches.DistinctBy(m => m.Message.Username).Count() < countThreshold)
+            return null;
+
         // matches need to relate to each other too.
         foreach (var match in matches.ToList())
         {
             var allMatch = matches
                 .Where(m => m != match)
-                .Select(m => m.Message)
-                .Select(m => new ScoredMessage(m, _algorithm.Distance(testMessage.Message.ToLowerInvariant(), m.Message.ToLowerInvariant())))
-                .All(ms => ms.Score <= distanceThreshold);
+                .Select(m => Distance(testMessage, m))
+                .All(ms => ms <= distanceThreshold);
 
             if (!allMatch)
                 matches.Remove(match);
         }
-        matches = matches.Append(new(testMessage, 0)).ToList();
-        if (matches.Count >= countThreshold)
-        {
-            return new(matches.First().Message, matches,
-                matches.Select(m => m.Message.TmiSentTs).Min(),
-                matches.Select(m => m.Message.TmiSentTs).Max());
-        }
-        return null;
+
+        matches.Add(testMessage);
+
+        // Counts must be distinct by username
+        if (matches.DistinctBy(m => m.Message.Username).Count() < countThreshold)
+            return null;
+
+        var group = new MessageGroup(matches,
+            matches.Select(m => m.Message.TmiSentTs).Min(),
+            matches.Select(m => m.Message.TmiSentTs).Max());
+
+        matches.ForEach(m => m.Group = group);
+
+        return group;
     }
 
-    public MessageGroup? TryCorrelateWithGroup(IChatMessage testMessage, MessageGroup group, double distanceThreshold)
+    public bool TryCorrelateWithGroup(CollectedMessage testMessage, MessageGroup group, double distanceThreshold)
     {
-        var allMatch = group.Messages
-            .Select(m => m.Message)
-            .Select(m => new ScoredMessage(m, _algorithm.Distance(testMessage.Message.ToLowerInvariant(), m.Message.ToLowerInvariant())))
-            .All(ms => ms.Score <= distanceThreshold);
+        var allMatch = group.Messages.All(m => Distance(testMessage, m) <= distanceThreshold);
         if (allMatch)
         {
-            return group with { Messages = group.Messages.Append(new(testMessage, 0)).ToList(), LastMessageAddedAt = testMessage.TmiSentTs };
+            testMessage.Group = group;
+            group.Messages.Add(testMessage);
+            group.LastMessageAddedAt = testMessage.Message.TmiSentTs;
+            return true;
         }
-        return null;
+        return false;
+    }
+
+    private double Distance(CollectedMessage messageA, CollectedMessage messageB)
+    {
+        return _algorithm.Distance(
+            messageA.Message.Message.ToLowerInvariant(), messageB.Message.Message.ToLowerInvariant());
     }
 }
